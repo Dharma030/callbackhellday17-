a9ec30f4d8e5e6e95d358945a5940dda8c062b79
DHARMA.S <112932573+Dharma030@users.noreply.github.com>
2022-11-18 17:24:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,77 @@
+setTimeout(()=>{
+    let heading = document.createElement("h1")
+    heading.innerHTML = 10
+    document.body.append(heading)
+
+},1000);
+
+setTimeout(()=>{
+    let heading1 = document.createElement("h1")
+    heading1.innerHTML = 9
+    document.body.append(heading1)
+
+},2000);
+
+
+setTimeout(()=>{
+    let heading2 = document.createElement("h1")
+    heading2.innerHTML = 8
+    document.body.append(heading2)
+
+},3000);
+
+setTimeout(()=>{
+    let heading3 = document.createElement("h1")
+    heading3.innerHTML = 7
+    document.body.append(heading3)
+
+},4000);
+
+setTimeout(()=>{
+    let heading4 = document.createElement("h1")
+    heading4.innerHTML = 6
+    document.body.append(heading4)
+
+},5000);
+
+setTimeout(()=>{
+    let heading5 = document.createElement("h1")
+    heading5.innerHTML = 5
+    document.body.append(heading5)
+
+},6000);
+
+setTimeout(()=>{
+    let heading6 = document.createElement("h1")
+    heading6.innerHTML = 4
+    document.body.append(heading6)
+
+},7000);
+
+setTimeout(()=>{
+    let heading7 = document.createElement("h1")
+    heading7.innerHTML = 3
+    document.body.append(heading7)
+
+},8000);
+
+setTimeout(()=>{
+    let heading8 = document.createElement("h1")
+    heading8.innerHTML = 2
+    document.body.append(heading8)
+
+},9000);
+
+setTimeout(()=>{
+    let heading9 = document.createElement("h1")
+    heading9.innerHTML = 1
+    document.body.append(heading9)
+
+},10000);
+
+setTimeout(()=>{
+    let heading10 = document.createElement("h1")
+    heading10.innerHTML = "HAPPY INDEPENDENCE DAY"
+    document.body.append(heading10)
+
+},11000);
